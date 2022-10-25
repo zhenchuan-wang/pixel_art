9ade09a166ce5fa80e4a2b49238253cbf42039d2
zhenchuan wang <zhenchuan.wang100@gmail.com>
2022-10-25 01:05:16 +0000

    Create pixel_art package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='pixel_art',
+   version='0.1',
+   description='Package for drawing pixel art to the terminal.',
+   author='Your name here',
+   packages=['pixel_art'],
+   install_requires = ['colorama']
+)
